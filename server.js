42f4ac4ae8c38cb36b8e7f39affc74810940b285
Pravin Rathod <150652249+PravinRathod21@users.noreply.github.com>
2025-04-20 17:15:11 +0530

    Update server.js
    Updated DB connection to use environment variables

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(express.json());
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "", // your password
-  database: "test", // put your DB name
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
